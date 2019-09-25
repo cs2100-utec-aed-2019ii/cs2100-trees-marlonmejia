266e9f1ef3c561e956de1ad0e8df1a864e073c37
marlon <marlon.mejia@utec.edu.pe>
2019-09-25 16:57:05 -0500

    feat(tree): insert and delete function

--- Tree.h
+++ Tree.h
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <stdlib.h>
+#include <conio.h>
 #include "Node.h"
 
 template<typename T>
@@ -9,17 +11,31 @@ public:
         this->root=nullptr;
     }
     ~Tree(){}
-    void new_Node(const T& value){
+    void Insert_Node(const T& value){
         Node<T>* node= new Node<T>(value);
         if(root==nullptr)
             root= node;
-        else
-        {
-            Node<T>* temp = root;
-
-        }
-           
-
+        else{
+           Node<T>* temp = root;
+            while(temp){
+                if (temp->key > _value)
+                    if(temp->left)
+                        temp = temp->left;
+                    else {
+                        temp->left = new_node;
+                        break;
+                    }
+                else if (temp->key < _value)
+                    if(temp->right)
+                        temp = temp->right;
+                    else {
+                        temp->right = new_node;
+                        break;
+                    }
+                else
+                    break;
+            }
+        }   
     }
     void delete_Node(const T& value){
     }
@@ -27,7 +43,17 @@ public:
     }
     Node<T>* max_Node(){
     }
-    int height_tree(){     
+    int height_tree(){
+        int heigth =0;
+        if(root==nullptr){
+            return heigth;
+        }     
+        else
+        {
+            Node<T>*Temp = root;
+            
+        }
+        
     }
     void Pre_orden(){}
     void In_orden(){}
